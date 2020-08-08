d8ff443eec819e303d312c0b3e5bf712f24d9690
behrooz pahlavan <pahlavan111@gmail.com>
2020-08-08 10:19:29 +0430

    complete enter phone page

--- app/src/main/java/com/bp/hamrahkhan/MainActivity.java
+++ app/src/main/java/com/bp/hamrahkhan/MainActivity.java
@@ -2,55 +2,68 @@ package com.bp.hamrahkhan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-
-import java.util.Timer;
-import java.util.TimerTask;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView img;
+    ImageView imgLocation;
+    EditText edtNumber;
+    Button btnGetCode;
+    String num="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        img = findViewById(R.id.imgcar);
-        playSound(getApplicationContext(),R.raw.car_honk);
 
-        Animation animation = AnimationUtils.loadAnimation(this,R.anim.enter_left);
-        img.setVisibility(View.VISIBLE);
-        img.startAnimation(animation);
-
+        setUpView();
+        startMyAnimations();
 
 
 
 
     }
 
-    public static void playSound(Context context, int soundID){
-        MediaPlayer mp = MediaPlayer.create(context, soundID);
-        Handler handler = new Handler();
-        Timer timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask() {
+    private String getNumber() {
+
+
+
+
+        return edtNumber.getText().toString();
+    }
+
+    private void setUpView() {
+        edtNumber = findViewById(R.id.editTextNumberPassword);
+        btnGetCode = findViewById(R.id.button);
+        imgLocation = findViewById(R.id.img_location);
+
+        btnGetCode.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                handler.post(() -> {
-                   mp.start();
-                    timer.cancel();
-                });
+            public void onClick(View v) {
+
+                String num =getNumber();
 
             }
-        }, 1500, 1);
-
-
+        });
     }
+
+    private void startMyAnimations() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom);
+        edtNumber.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom_slow);
+        btnGetCode.startAnimation(animation);
+        animation= AnimationUtils.loadAnimation(this, R.anim.blink);
+        imgLocation.startAnimation(animation);
+    }
+
 }
