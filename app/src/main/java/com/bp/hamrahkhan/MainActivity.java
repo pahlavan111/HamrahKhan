b80709ea3d3bd5f720f710b99fad81d061ae9792
behrooz pahlavan <pahlavan111@gmail.com>
2020-08-09 08:31:01 +0430

    complete get code proses and animations

--- app/src/main/java/com/bp/hamrahkhan/MainActivity.java
+++ app/src/main/java/com/bp/hamrahkhan/MainActivity.java
@@ -3,8 +3,9 @@ package com.bp.hamrahkhan;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,6 +13,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bp.hamrahkhan.data.ApiClient;
@@ -26,11 +29,15 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean x = true;
+    TextView txtDesc,txtEditNum;
     ImageView imgLocation;
-    EditText edtNumber,edtCode;
-    Button btnGetCode,btnSendCode;
-    LinearLayout linearSendNum,linearGetCode;
+    EditText edtNumber, edtCode;
+    Button btnGetCode, btnSendCode;
+    LinearLayout linearSendNum, linearGetCode;
+    RelativeLayout rlTimerContainer;
     private static final String API_KEY = "0b49ad807b3c63860558cef5d1a6b46bc49afcf7";
+    AlertDialog dialog;
 
 
     @Override
@@ -39,22 +46,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUpView();
-        startMyAnimations();
+        turnToSendNumMode();
+
         btnGetCode.setOnClickListener(v -> {
 
-            sendSms();
+            if (checkMobileNumber()) {
+                sendSms();
 
+
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+                final LayoutInflater inflater= LayoutInflater.from(MainActivity.this);
+                View view= inflater.inflate(R.layout.dialog,null);
+                builder.setView(view);
+                dialog=builder.create();
+                dialog.show();
+            }
         });
 
+        txtEditNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                turnToSendNumMode();
+            }
+        });
 
 
     }
 
+    private boolean checkMobileNumber() {
+
+        String num = edtNumber.getText().toString();
+        if (num.length() == 10) {
+            return true;
+        } else {
+            edtNumber.setError("شماره به صورت صحیح وارد نشده است!");
+            return false;
+        }
+    }
+
     private void sendSms() {
 
-        Long num=Long.parseLong(edtNumber.getText().toString());
-        ApiService service=ApiClient.getClient().create(ApiService.class);
-        service.login(new MobileSend(num,API_KEY)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new SingleObserver<MobileSendResponse>() {
+        Long num = Long.parseLong(edtNumber.getText().toString());
+        ApiService service = ApiClient.getClient().create(ApiService.class);
+        service.login(new MobileSend(num, API_KEY)).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new SingleObserver<MobileSendResponse>() {
             @Override
             public void onSubscribe(Disposable d) {
 
@@ -63,13 +97,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(MobileSendResponse mobileSendResponse) {
 
-                Toast.makeText(MainActivity.this, mobileSendResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                switch (mobileSendResponse.getCode()) {
+                    case 200:
+                        turnToGetCodeMode();
+                        dialog.dismiss();
+                        break;
 
-                turnToGetCodeMode();
-//                if (mobileSendResponse.getData().isMobileValidation()){
-//
-//                    Toast.makeText(MainActivity.this, "fffffffffff", Toast.LENGTH_SHORT).show();
-//                }
+                    case 400:
+                    case 503:
+                    case 403:
+                    case 402:
+                        Toast.makeText(MainActivity.this, mobileSendResponse.getCode() + " -> " + mobileSendResponse.getMessage(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
 
             }
 
@@ -84,32 +125,50 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUpView() {
 
+        txtDesc = findViewById(R.id.txt_desc);
         imgLocation = findViewById(R.id.img_location);
         edtNumber = findViewById(R.id.editTextNumberPassword);
         edtCode = findViewById(R.id.editGetCode);
-        btnGetCode = findViewById(R.id.button);
+        btnGetCode = findViewById(R.id.btn_send_num);
         btnSendCode = findViewById(R.id.btn_send_code);
         linearSendNum = findViewById(R.id.ll_send_sms);
         linearGetCode = findViewById(R.id.ll_get_sms);
+        rlTimerContainer = findViewById(R.id.rl_timer_container);
+        txtEditNum = findViewById(R.id.txt_edit_num);
 
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.blink);
+        imgLocation.startAnimation(animation);
 
     }
 
-    private void startMyAnimations() {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom);
+    private void turnToSendNumMode() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        linearGetCode.startAnimation(animation);
+        linearGetCode.setVisibility(View.GONE);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        txtDesc.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom);
         edtNumber.startAnimation(animation);
         animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom_slow);
         btnGetCode.startAnimation(animation);
-        animation= AnimationUtils.loadAnimation(this, R.anim.blink);
-        imgLocation.startAnimation(animation);
+        linearSendNum.setVisibility(View.VISIBLE);
     }
 
 
-    void turnToGetCodeMode(){
-
+    void turnToGetCodeMode() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        linearSendNum.startAnimation(animation);
         linearSendNum.setVisibility(View.GONE);
-        linearGetCode.setVisibility(View.VISIBLE);
+        animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        rlTimerContainer.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom);
+        edtCode.startAnimation(animation);
+        animation = AnimationUtils.loadAnimation(this, R.anim.enter_bottom_slow);
+        btnSendCode.startAnimation(animation);
 
+        linearGetCode.setVisibility(View.VISIBLE);
     }
+
 
 }
